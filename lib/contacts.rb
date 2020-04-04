b4b999617ff2c759ddf54eebb43a513c4102ff82
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 08:29:37 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,13 +15,15 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
+ contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-      binding.pry
+      if attribute == :favorite_ice_cream_flavors
+        binding.pry
+      end
     end
   end
- end
+end
 end
 
 
